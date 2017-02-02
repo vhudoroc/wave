0d5297684c10277a3ce821e8a327e9743a2990fa
vhudoroc <vhudoroc@yahoo.com>
2017-02-01 19:54:40 -0500

    add aws-app.sh

---
+++ aws-app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#set -x
+set -e
+
+
+wget https://releases.hashicorp.com/terraform/0.8.5/terraform_0.8.5_linux_amd64.zip
+
+unzip terraform_0.8.5_linux_amd64.zip -d /usr/local/sbin
+
+terraform apply
